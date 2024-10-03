703c2ab06bfc17a5c7247175327b7c6dc816fe13
ib823 <ikmal.baharudin@gmail.com>
2024-10-03 14:59:07 +0000

    Update deployment for GitHub Pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,11 @@ import bursaLogo from '../assets/images/bursa-logo.png';
 function Header() {
   return (
     <header className="app-header">
+      <div className="header-title">BURSA Malaysia ERP Transformation</div>
       <div className="logo-container">
         <img src={abeamLogo} alt="ABeam Malaysia Logo" className="abeam-logo" />
         <img src={bursaLogo} alt="Bursa Malaysia Logo" className="bursa-logo" />
       </div>
-      <h1>BURSA Malaysia ERP Transformation</h1>
     </header>
   );
 }
